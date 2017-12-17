42161e438038494c0477a099500e37040dfcc264
Rewaz Esmaeel <rewaz.esmaeel@kcl.ac.uk>
2017-12-17 19:33:33 +0000

    added node_modules to .gitignore

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const express = require('express');
-
-const app = express();
-
-const cors = require('cors');
-
-const server = app.listen(4000, function()
-{
-	console.log('port 4000 is being listened to right now')
+var express = require('express');
+var cors = require("cors");
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+var users = ["Joe","Luke","Rewaz","Stefan"];
+
+var data = 
+
+
+app.locals.users = users;
+
+/*app.use(function(req,res,next)
+{
+	console.log(`${req.method} request for '${req.url}'`);
+});*/
+
+
 
 app.use(cors());
 
-app.get('/', (req, res) =>  res.send('Hello World!'))
+app.use(express.static("./main"))
+
+app.get('/', function (req, res) {
+		res.sendFile(__dirname + '/index.html');
+  });
 
 
 
+app.get('/data-api', function (req, res) {
+		res.json(users);
+  });
+
+
+var server = app.listen(8000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
